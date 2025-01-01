cff311c1d328fc72f3b84f53b5d5a2c4a308d1c4
Mohammad Faraj <76161034+MoFa-14@users.noreply.github.com>
2025-01-01 06:46:42 -0800

    Update syscalls.c

--- Lab4.2/Src/syscalls.c
+++ Lab4.2/Src/syscalls.c
@@ -11,7 +11,7 @@
 **
 **  Environment : System Workbench for MCU
 **
-**  Distribution: The file is distributed “as is,” without any warranty
+**  Distribution: The file is distributed Â“as is,Â” without any warranty
 **                of any kind.
 **
 **  (c)Copyright System Workbench for MCU.
@@ -36,168 +36,128 @@
 #include <sys/times.h>
 #include <sys/unistd.h>
 
-
 /* Variables */
-//#undef errno
 extern int errno;
-#define FreeRTOS
-#define MAX_STACK_SIZE 0x2000
-
-extern int __io_putchar(int ch) __attribute__((weak));
-extern int __io_getchar(void) __attribute__((weak));
-
-#ifndef FreeRTOS
-  register char * stack_ptr asm("sp");
-#endif
-
-
-register char * stack_ptr asm("sp");
-
-char *__env[1] = { 0 };
+register char *stack_ptr asm("sp");
+char *__env[1] = {0};
 char **environ = __env;
 
+// Expose heap_end for external use
+char *heap_end = NULL;
 
 /* Functions */
-void initialise_monitor_handles()
-{
-}
+void initialise_monitor_handles() {}
 
-int _getpid(void)
-{
-	return 1;
-}
+int _getpid(void) { return 1; }
 
 int _kill(int pid, int sig)
 {
-	errno = EINVAL;
-	return -1;
+    errno = EINVAL;
+    return -1;
 }
 
-void _exit (int status)
+void _exit(int status)
 {
-	_kill(status, -1);
-	while (1) {}		/* Make sure we hang here */
+    _kill(status, -1);
+    while (1)
+    {
+    } /* Hang here */
 }
 
-int _read (int file, char *ptr, int len)
+int _read(int file, char *ptr, int len)
 {
-	/* The I/O library uses an internal buffer */
-	/* It asks for 1024 characters even if only getc() is used. */
-	/* If we use a for(;;) loop on the number of characters requested, */
-	/* the user is forced to enter the exact number requested, even if only one is needed. */
-	/* So here we return only 1 character even if requested length is > 1 */
-	*ptr = __io_getchar();
-
-	return 1;
+    *ptr = __io_getchar();
+    return 1;
 }
 
 int _write(int file, char *ptr, int len)
 {
-int DataIdx;
-switch (file) 
-  {     
-  case STDOUT_FILENO: /* stdout */
-  case STDERR_FILENO: /* stderr */
-    for (DataIdx = 0; DataIdx < len; DataIdx++)
-      {
-__io_putchar(*ptr++);
-      }
-    break;
-    default:       /* error no such file stream */
-      errno = EBADF;       
-      return -1;
-  }
-return len;
+    int DataIdx;
+    switch (file)
+    {
+    case STDOUT_FILENO: /* stdout */
+    case STDERR_FILENO: /* stderr */
+        for (DataIdx = 0; DataIdx < len; DataIdx++)
+        {
+            __io_putchar(*ptr++);
+        }
+        break;
+    default:
+        errno = EBADF;
+        return -1;
+    }
+    return len;
 }
 
 caddr_t _sbrk(int incr)
 {
-	extern char end asm("end");
-	static char *heap_end;
-	char *prev_heap_end;
+    extern char end asm("end"); // Defined by the linker
+    char *prev_heap_end;
 
-	if (heap_end == 0)
-		heap_end = &end;
+    if (heap_end == 0)
+        heap_end = &end;
 
-	prev_heap_end = heap_end;
-	if (heap_end + incr > stack_ptr)
-	{
-//		write(1, "Heap and stack collision\n", 25);
-//		abort();
-		errno = ENOMEM;
-		return (caddr_t) -1;
-	}
+    prev_heap_end = heap_end;
 
-	heap_end += incr;
+    if (heap_end + incr > stack_ptr) // Check for stack collision
+    {
+        errno = ENOMEM;
+        return (caddr_t)-1;
+    }
 
-	return (caddr_t) prev_heap_end;
+    heap_end += incr;
+
+    return (caddr_t)prev_heap_end;
 }
 
-int _close(int file)
-{
-	return -1;
-}
-
+int _close(int file) { return -1; }
 
 int _fstat(int file, struct stat *st)
 {
-	st->st_mode = S_IFCHR;
-	return 0;
+    st->st_mode = S_IFCHR;
+    return 0;
 }
 
-int _isatty(int file)
-{
-	return 1;
-}
+int _isatty(int file) { return 1; }
 
-int _lseek(int file, int ptr, int dir)
-{
-	return 0;
-}
+int _lseek(int file, int ptr, int dir) { return 0; }
 
-int _open(char *path, int flags, ...)
-{
-	/* Pretend like we always fail */
-	return -1;
-}
+int _open(char *path, int flags, ...) { return -1; }
 
 int _wait(int *status)
 {
-	errno = ECHILD;
-	return -1;
+    errno = ECHILD;
+    return -1;
 }
 
 int _unlink(char *name)
 {
-	errno = ENOENT;
-	return -1;
+    errno = ENOENT;
+    return -1;
 }
 
-int _times(struct tms *buf)
-{
-	return -1;
-}
+int _times(struct tms *buf) { return -1; }
 
 int _stat(char *file, struct stat *st)
 {
-	st->st_mode = S_IFCHR;
-	return 0;
+    st->st_mode = S_IFCHR;
+    return 0;
 }
 
 int _link(char *old, char *new)
 {
-	errno = EMLINK;
-	return -1;
+    errno = EMLINK;
+    return -1;
 }
 
 int _fork(void)
 {
-	errno = EAGAIN;
-	return -1;
+    errno = EAGAIN;
+    return -1;
 }
 
 int _execve(char *name, char **argv, char **env)
 {
-	errno = ENOMEM;
-	return -1;
+    errno = ENOMEM;
+    return -1;
 }
